25cd2d44ed84e3ba6f8fb7881c6f7df5bac8ded0
neham3895 <neham3895@gmail.com>
2017-01-17 10:54:26 -0500

    ExploratoryDataAnalysis-Project 1

--- Plot1.R
+++ Plot1.R
@@ -2,7 +2,7 @@
 data <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(data) <- c("Date","Time","Global_active_power","Global_reactive_power",
     "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subdata <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
+subdata <- subset(data,data$Date=="4/2/2007" | data$Date =="5/2/2007")
 
 #calling the basic plot function
 subdata$Global_active_power<-as.numeric(as.character(subdata$Global_active_power))
